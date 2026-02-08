7f6aa576a868af051a199e9fe521c60ef39e73c6
SADEWAMEERAGOPAL <SADEWADMEERA76@GMAIL.COM>
2026-02-08 15:10:41 +0530

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,15 @@ export class AppComponent {
 
 title1= 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat labore officiis fuga! Explicabo rerum iure, illum, quas harum autem voluptatibus doloribus quasi neque aut at tenetur quo possimus, quae qui.'
 
-
+courseInfo={
+    name: "Mean  Stack Development",
+    durations: "6 Month",
+    fees: 20000,
+    trainer: "Ravi Sir",
+    startDate: new Date(2025, 4),
+    rating: 4.523456,
+    students: 14500000
+  }
 
 playesraArr: Iplayers[]=playersdata
 }
--- src/app/directives/advance-csshighlitghter.directive.ts
+++ src/app/directives/advance-csshighlitghter.directive.ts
@@ -1,9 +1,17 @@
-import { Directive, ElementRef, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appAdvanceCsshighlitghter]'
 })
 export class AdvanceCsshighlitghterDirective implements OnInit {
+  @Input() defaultColor: string= 'red'
+  @Input() hovercolor: string= 'green'
+
+  
+
+  @HostBinding('style.backgroundColor')
+    bgColor: string= this.defaultColor
+
  @Input() getstyle: string='yellow'
   @Input() getclass: string='alert-info'
   constructor(private _eleRef: ElementRef,
@@ -21,6 +29,22 @@ export class AdvanceCsshighlitghterDirective implements OnInit {
     this._rendere.setStyle(this._eleRef.nativeElement, 'background-color', 'pink')
   }
 
+  @HostListener('mouseover')
+onMouseHover() {
+  // this._renderer.setStyle(
+  //   this._eleRef.nativeElement,
+  //   'background-color',
+  //   'orange'
+  // );
 
+  this.bgColor=this.hovercolor
+}
+
+@HostListener('mouseout')
+onMouseOut(){
+  this._rendere.setStyle(this._eleRef.nativeElement, 'background-color', this.defaultColor)
+
+  this.bgColor=this.hovercolor
+}
   
 }
